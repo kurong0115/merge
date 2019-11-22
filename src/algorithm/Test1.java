f7b25490818d80ca7d1d4e5178ef92bef2ca3d5e
kurong0115 <1181209156@qq.com>
2019-11-22 11:08:55 +0800

    1

---
+++ src/algorithm/Test1.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+/**
+ * @ClassName Test1
+ * @Description master
+ * @Author Administrator
+ * @Date 2019/11/22 11:08
+ * @Version 1.0
+ */
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
