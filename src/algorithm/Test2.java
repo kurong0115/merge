f37e1d250ab81cc7c817975ce076c31108492e1f
kurong0115 <1181209156@qq.com>
2019-11-22 11:19:02 +0800

    power

---
+++ src/algorithm/Test2.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+/**
+ * @ClassName Test2
+ * @Description 添加功能
+ * @Author Administrator
+ * @Date 2019/11/22 11:17
+ * @Version 1.0
+ */
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
